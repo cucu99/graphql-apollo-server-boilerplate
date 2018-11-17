86c8b435a9c3a6d40cd9dea0a5306de98ebe5f43
Daniel Kirchner <cucu851223@gmail.com>
2018-11-17 21:55:26 +0100

    babel fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import 'dotenv/config';
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
-TODO:
-const schema = gql``;
-const resolvers = {};
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Daniel Kirchner'
+      };
+    }
+  }
+};
 
 const server = new ApolloServer({
   typeDefs: schema,
@@ -14,6 +31,6 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, path: '/grqphql' });
 
-app.listen({ port:8000 }, () => {
+app.listen({ port: 8000 }, () => {
   console.log('Apollo Server on http://localhost:8000/graphql');
 });
