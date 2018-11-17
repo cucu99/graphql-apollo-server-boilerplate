e5761b46137833dbdaeaaddbe6541d239f92c57a
Daniel Kirchner <cucu851223@gmail.com>
2018-11-17 16:17:41 +0100

    apollo-express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import 'dotenv/config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
-console.log('Hello');
+const app = express();
+TODO:
+const schema = gql``;
+const resolvers = {};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+});
+
+server.applyMiddleware({ app, path: '/grqphql' });
+
+app.listen({ port:8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
