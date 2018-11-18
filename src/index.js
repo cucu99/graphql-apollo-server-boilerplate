b9a4c2157b1dae066586318d8011d6a12df6d89d
Daniel Kirchner <cucu851223@gmail.com>
2018-11-18 22:34:46 +0100

    cors, graphql schema types querys and resolvers

--- src/index.js
+++ src/index.js
@@ -1,25 +1,58 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
 
+app.use(cors());
+
 const schema = gql`
   type Query {
+    users: [User!]
+    user(id: ID!): User
     me: User
   }
 
   type User {
+    id: ID!
     username: String!
+    firstname: String!
+    lastname: String!
+    age: Int!
   }
 `;
 
+//Temporary data
+let users = {
+  1: {
+    id: '1',
+    username: 'Daniel Kirchner',
+    firstname: 'Daniel',
+    lastname: 'Kirchner',
+    age: '32'
+  },
+  2: {
+    id: '2',
+    username: 'Teszt Elek',
+    firstname: 'Teszt',
+    lastname: 'Elek',
+    age: '25'
+  }
+};
+
+const me = users[1];
+
 const resolvers = {
   Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
     me: () => {
-      return {
-        username: 'Daniel Kirchner'
-      };
+      return me;
     }
   }
 };
@@ -29,7 +62,7 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.applyMiddleware({ app, path: '/grqphql' });
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen({ port: 8000 }, () => {
   console.log('Apollo Server on http://localhost:8000/graphql');
